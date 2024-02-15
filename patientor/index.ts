e5a397e83c28153e59ad964ccf8381e16f8aabec
Federico Re <federico.re1996@gmail.com>
2024-02-15 17:38:04 +0300

    ok

--- patientor/index.ts
+++ patientor/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.get("/api/ping", (_req, resp) => {
+  console.log("im here");
   resp.send("pong");
 });
 
-const PORT = 3003;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
