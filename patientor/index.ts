e750a44ca6ad3621f096321e73a3c487bd8c1f53
Federico Re <federico.re1996@gmail.com>
2024-02-15 12:13:58 +0300

    starting

--- patientor/index.ts
+++ patientor/index.ts
@@ -1,9 +1,12 @@
-import express from "express"
+import express from "express";
 
-const app = express()
+const app = express();
+app.get("/api/ping", (_req, resp) => {
+  resp.send("pong");
+});
 
-const PORT = 3003
+const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
